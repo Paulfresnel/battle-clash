3db2d4ec7ed2619181aadc87346fda9f12baf6d2
Paulfresnel <paul.fresnel1@gmail.com>
2022-10-22 14:56:51 +0200

    updated script logic
    Fully working and implemented for right facing soldiers

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 let gameInterval, shooterRightInterval;
 const rightCharBullets = [];
+const rightCharSoldiers = [];
+const lefttCharSoldiers = [];
+const leftCharBullets = [];
 
 const myGameArea = {
   frames:0,
@@ -49,7 +52,9 @@ window.onload = function () {
     canvas.width = width;
     canvas.height = height;
     unitTestLeft.draw();
-    unitTestRight.draw();
+    if (rightCharSoldiers.length>0){ 
+      rightCharSoldiers[0].draw();
+    }
     gameOnGoing();
   };
 
@@ -61,20 +66,48 @@ function renderGame(){
     myGameArea.frames +=1;
     ctx.clearRect(0,0,600,600);
     unitTestLeft.moveLeft();
-    unitTestRight.moveRight();
+    updateSoldiersRight();
     updateBullets();
     rightCharacterBulletsCollision();
     checkIfDead();
+    
 }
 
 
 
-class Projectiles {
+class ProjectilesRight {
   constructor(){
-    this.x = unitTestRight.x + 48;
+    this.x = rightCharSoldiers[0].x + 48;
     this.y = 22;
     this.width = 10;
     this.height = 3;
+    this.damage = 5;
+  }
+  draw(){
+    ctx.fillRect(this.x ,this.y, this.width,this.height);
+  }
+  left(){
+    return this.x;
+  } right(){
+    return this.x + this.width;
+  } bottom(){
+    return this.y + this.height;
+  } top(){
+    return this.y;
+  }
+//  moveRight(){
+//    this.x += 20;
+//    this.draw();
+//}
+}
+
+class ProjectilesLeft {
+  constructor(){
+    this.x = rightCharSoldiers[0].x + 48;
+    this.y = 22;
+    this.width = 10;
+    this.height = 3;
+    this.damage = 5;
   }
   draw(){
     ctx.fillRect(this.x ,this.y, this.width,this.height);
@@ -154,6 +187,8 @@ class blackUnitLeft {
       this.width = 48;
       this.height = 48;
       this.health = 10,
+      this.speed = 0;
+      this.state = false;
         this.rangedAttack = 5;
         this.meleeAttack = 2;
       const imageLeft = new Image();
@@ -166,6 +201,7 @@ class blackUnitLeft {
   }  
   draw() {
     ctx.drawImage(this.image, this.sX, this.sY, this.sW, this.sH, this.x ,this.y, this.width,this.height);
+    //if (state === running);
 
   }
   drawCrouch(){
@@ -182,11 +218,16 @@ class blackUnitLeft {
   }
   moveLeft(){
     this.x  -= 1;
+    //if(this.speed >3){
+    //  imageLeft.src = './images/TeamGunner_By_SecretHideout_060519/CHARACTER_SPRITES/Black/Gunner_Black_Run.png';
+    //  ctx.drawImage(this.image,48, 0, this.sW, this.sH, this.x ,this.y,this.width,this.height)
+    //  unitTestLeft.draw()
+    //}
     unitTestLeft.draw()
    
   }
   left(){
-    return this.x;
+    return this.x +10;
   } right(){
     return this.x + this.width;
   } bottom(){
@@ -196,25 +237,39 @@ class blackUnitLeft {
   }
 }
 
-const unitTestRight = new blackUnitRight();
-
+const unitTestRight = rightCharSoldiers[0];
 const unitTestLeft = new blackUnitLeft(96,0);
 
 
+function updateSoldiersRight(){
+  for (i = 0; i <  rightCharSoldiers.length; i++) {
+    if (myGameArea.frames % 1 === 0){
+      rightCharSoldiers[i].x += 0.5;
+      rightCharSoldiers[i].draw();
+     // ctx.clearRect(rightCharBullets[i].x,rightCharBullets[i].y,rightCharBullets[i].width,rightCharBullets[i].height);
+      }// if (myGameArea.frames % 100 === 0){
+      //  rightCharBullets[i].x += -3;
+      //  rightCharBullets[i].draw();
+      //}
+  }
+  if (myGameArea.frames % 1 === 0 && rightCharSoldiers.length < 1) { // every 3s
+    rightCharSoldiers.push(new blackUnitRight());//
+  }
+}
+
 function updateBullets(){
     for (i = 0; i <  rightCharBullets.length; i++) {
-      if (myGameArea.frames % 1 === 0){
+      if (myGameArea.frames % 1 === 0 ){
         rightCharBullets[i].x += 5;
         rightCharBullets[i].draw();
        // ctx.clearRect(rightCharBullets[i].x,rightCharBullets[i].y,rightCharBullets[i].width,rightCharBullets[i].height);
-        }// if (myGameArea.frames % 100 === 0){
-        //  rightCharBullets[i].x += -3;
-        //  rightCharBullets[i].draw();
-        //}
-    }
-    if (myGameArea.frames % 100 === 0) { // every 3s
-      rightCharBullets.push(new Projectiles());//
-    }
+      }
+      
+    }if (myGameArea.frames % 100 === 0) {
+        console.log('working?') // every 3s
+        rightCharBullets.push(new ProjectilesRight());//
+      }
+    
 }
 
 function rightCharacterBulletsCollision() {
@@ -222,9 +277,9 @@ function rightCharacterBulletsCollision() {
   if (rightCharBullets.length > 0){
   for(i=0;i<rightCharBullets.length;i++){
       if (rightCharBullets[i].right() >= unitTestLeft.left()) {
-        rightCharBullets.splice(i,1); // if there’s a collision
         alert('lost');
-        unitTestLeft.health -= unitTestRight.rangedAttack;
+        unitTestLeft.health -= rightCharBullets[i].damage;
+        rightCharBullets.splice(i,1); 
         console.log(unitTestLeft.health)
         //let points = rightCharBullets[i].points;
         //  unitTestLeft.balance += points; // remove the Points from player’s Balance
